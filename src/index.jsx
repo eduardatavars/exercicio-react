9d798f8bb67ed0aa8b57bd0bc02b3806ce8c92be
eduardatavars <mariaeduardtavares@gmail.com>
2024-10-31 18:04:00 -0300

    conclusão do exercicio

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import Formulario from './components/Formulario';
+import App from './App.jsx'
 import "./index.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Formulario />
+    <App />
   </StrictMode>
 );
